fe5dced20635c99cf5e7be8f90efd265f09b0b24
Reynante Matias <reynante.matias09@gmail.com>
2015-06-26 14:15:45 -0700

    Modified code

--- src/Hooptie.java
+++ src/Hooptie.java
@@ -5,70 +5,60 @@
  */
 
 import java.util.Scanner;
+import java.lang.String;
 
 public class Hooptie {
     public static void main(String[] args) {
-        //declaration and initialization
         Scanner input = new Scanner(System.in);
 
-        int n1=35, n2=65, n3=20, result=0, result1=0, result2=0, base, total;
+        String name = "";
+        int price = 0, withPep = 0, ifRepeat = 0, base, total;
 
-        //asks for user input
         System.out.println("Available Cars: 1 for Econobox, 2 for Whip, 3 for Junker");
         System.out.print("Please select a rental car: ");
         int car = input.nextInt();
 
         System.out.print("Please enter how many days of rental: ");
-        int rental = input.nextInt();
+        int days = input.nextInt();
 
         System.out.print("Platinum Executive Package? 1 for yes, 0 for no? ");
-        int PEP = input.nextInt();
+        int pep = input.nextInt();
 
         System.out.print("Are you a repeat customer? 1 for yes, 0 for no. ");
         int repeat = input.nextInt();
-        System.out.println("");
+        System.out.println("\n");
 
-        //checks user input and calculates
-        base = (result * rental);
-        if (car == 1){
-            result = n1;
-            base = (result * rental);
-            System.out.print("Base: " + rental + " days for a Econobox @ $" + result + " per day:");
-            System.out.println("          $" + base);
-        } else if (car == 2){
-            result = n2;
-            base = (result * rental);
-            System.out.print("Base: " + rental + " days for a Whip @ $" + result + " per day:");
-            System.out.println("              $" + base);
-        } else if (car == 3){
-            result = n3;
-            base = (result * rental);
-            System.out.print("Base: " + rental + " days for a Junker @ $" + result + " per day:");
-            System.out.println("            $" + base);
+        if (car == 1) {
+            price = 35;
+            name = "Econobox";
+        } else if (car == 2) {
+            price = 65;
+            name = "Whip";
+        } else if (car == 3) {
+            price = 20;
+            name = "Junker";
         }
 
-        //if platinum executive package
-        if (PEP == 1){
-            result1 = ((int) (base * .1));
+        base = (price * days);
+
+        System.out.print("Base: " + days + " days for a " + name + " @ $" + price + " per day:");
+        System.out.println("\t$" + base);
+
+        if (pep == 1) {
+            withPep = ((int) (base * .1));
             System.out.print("Platinum Executive Package:");
-            System.out.println("                         +$" + result1);
-        } else {
-
+            System.out.println("\t\t\t\t\t$" + withPep);
         }
 
-        //if repeat
-        if (repeat == 1){
-            result2 = ((int) (result * (rental/5)));
+        if (repeat == 1) {
+            ifRepeat = price * (days/5);
             System.out.print("Repeat Customer Discount:");
-            System.out.println("                           -$" + result2);
-        } else {
-
+            System.out.println("\t\t\t\t\t-$" + ifRepeat);
         }
 
-        //print
         System.out.println("");
-        total = ((base + result1) - result2);
+        total = ((base + withPep) - ifRepeat);
         System.out.print("Total Estimate for Rental:");
-        System.out.println("                           $" + total);
+        System.out.println("\t\t\t\t\t$" + total);
     }
 }
